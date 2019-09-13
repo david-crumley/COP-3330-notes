4b6d7e5a906d7340d34851fbabc08bde21bd9ef5
david-crumley <david_crumley@knights.ucf.edu>
2019-09-12 20:12:43 -0400

    added ch_2_notes

---
+++ ch2_notes/src/ch2_notes/math.java
@@ -0,0 +1,16 @@
+package ch2_notes;
+
+public class math {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int areaSquare = 81;
+		double square;
+		
+		
+		square = Math.sqrt(areaSquare);
+
+		System.out.println(square);
+	}
+
+}
